3129db9af0e7e999595c4ad0abd697879c1b8866
m_v_p <bmikel@mail.ru>
2019-11-21 19:44:50 +0400

    add one test

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,7 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('some good test', () => {
+  expect(true).toBeTruthy();
+})
